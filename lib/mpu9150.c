9c8f2cdbb388c084aafb0c60d6fbeb61d4c40593
Nathan Lilienthal <nathan@nixpulvis.com>
2015-10-11 17:54:16 -0400

    Use new i2c functions

--- lib/mpu9150.c
+++ lib/mpu9150.c
@@ -1,7 +1,5 @@
 #include "mpu9150.h"
 
 byte mpu9150_get_device_id() {
-    byte buffer;
-    i2c_read_register(0x68, 0x75, &buffer, 1);
-    return buffer;
+    return i2c_read_register_byte(0x68, 0x75);
 }
